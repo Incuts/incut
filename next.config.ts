fbae1538e33a603463a55c506cf6c6283c2a40d5
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-11-25 00:38:51 +0545

    add: changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "/incut/"
+  assetPrefix: process.env.ASSET_PREFIX || "/incut/"
 };
 
 export default nextConfig;
