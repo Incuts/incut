f73160de767678a9335523eed2d3f856fc1fb08e
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-11-26 01:28:10 +0545

    fix: lock file issue and some other things

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "export",
+  assetPrefix: process.env.ASSET_PREFIX || "/incut/",
+  images:{
+    unoptimized:true
+  }
+
 }
  
 module.exports = nextConfig
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  assetPrefix: process.env.ASSET_PREFIX || "/incut/"
-};
-
-export default nextConfig;
