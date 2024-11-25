4eacdb3401035184ca0b97656ef9dce97b3194d6
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-11-26 01:15:59 +0545

    fix: lint changes

--- src/modules/Recent/presentation/page.tsx
+++ src/modules/Recent/presentation/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { CarouselSection } from "../components";
 
 const page = () => {
   return (
--- src/modules/VideoTypes/presentation/page.tsx
+++ src/modules/VideoTypes/presentation/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div className="px-5 flex flex-col justify-center items-center gap-10 mb-10 lg:mb-20 text-center">
       <p className="text-4xl font-semibold p-5">
-        Whatever you need, we've got an editor for it.
+        Whatever you need, we&apos;ve got an editor for it.
       </p>
       <FeatureGrid />
     </div>
